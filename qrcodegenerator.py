ac7c9675522180b4d2d355ff239f14da95650162
akash9upadhyay <92147018+akash9upadhyay@users.noreply.github.com>
2025-08-30 20:06:45 +0530

    Add files via upload

---
+++ qrcodegenerator.py
@@ -0,0 +1,23 @@
+import qrcode
+
+#taking the input from the user
+upi_id=input("Enter your upi id :")
+
+phonepe_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+#create the qr code
+phonepe_qr=qrcode.make(phonepe_url)
+paytm_qr=qrcode.make(paytm_url)
+google_pay_qr=qrcode.make(google_pay_url)
+
+#save the qr to the files (optional)
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+#display the qr code you may need to install pillow library
+
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
